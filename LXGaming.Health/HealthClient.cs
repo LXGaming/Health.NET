6d01a3220225d1d6942539457589acdacd043a3f
Alex Thomson <LXGaming@users.noreply.github.com>
2022-08-10 18:34:28 +1200

    Asynchronous Client

--- LXGaming.Health/HealthClient.cs
+++ LXGaming.Health/HealthClient.cs
@@ -1,6 +1,7 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
-using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.Extensions.Logging;
 
 namespace LXGaming.Health {
@@ -8,12 +9,15 @@ namespace LXGaming.Health {
     public abstract class HealthClient : Health {
 
         private readonly byte[] _buffer = new byte[1];
+        private readonly ManualResetEvent _event = new ManualResetEvent(false);
+        private bool _disposed;
 
         protected HealthClient(ILogger<HealthClient> logger, EndPoint endPoint) : base(logger, endPoint) {
         }
 
         public override void Start() {
-            Socket.Connect(EndPoint);
+            _event.Reset();
+            Socket.BeginConnect(EndPoint, ConnectCallback, Socket);
         }
 
         public override void Stop() {
@@ -22,11 +26,77 @@ namespace LXGaming.Health {
         }
 
         public override bool GetStatus() {
-            return Socket.Receive(_buffer, SocketFlags.None) == 1 && _buffer[0] == Healthy;
+            _event.WaitOne();
+            return _buffer.Length == 1 && _buffer[0] == Healthy;
         }
 
-        public async Task<bool> GetStatusAsync() {
-            return await Socket.ReceiveAsync(_buffer, SocketFlags.None) == 1 && _buffer[0] == Healthy;
+        private void ConnectCallback(IAsyncResult result) {
+            var client = (Socket) result.AsyncState;
+            if (client == null) {
+                Logger.LogError("Client is unavailable");
+                return;
+            }
+
+            try {
+                client.EndConnect(result);
+            } catch (Exception ex) {
+                Logger.LogError(ex, "Encountered an error while ending connect");
+                return;
+            }
+
+            try {
+                client.BeginReceive(_buffer, 0, 1, SocketFlags.None, ReceiveCallback, client);
+            } catch (Exception ex) {
+                Logger.LogError(ex, "Encountered an error while beginning receive from {Server}", client.RemoteEndPoint.ToString());
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult result) {
+            var client = (Socket) result.AsyncState;
+            if (client == null) {
+                Logger.LogError("Client is unavailable");
+                return;
+            }
+
+            try {
+                client.EndReceive(result);
+            } catch (Exception ex) {
+                Logger.LogError(ex, "Encountered an error while ending receive from {Server}", client.RemoteEndPoint.ToString());
+            }
+
+            try {
+                client.BeginDisconnect(true, DisconnectCallback, client);
+            } catch (Exception ex) {
+                Logger.LogError(ex, "Encountered an error while beginning disconnect from {Server}", client.RemoteEndPoint.ToString());
+            }
+        }
+
+        private void DisconnectCallback(IAsyncResult result) {
+            var client = (Socket) result.AsyncState;
+            if (client == null) {
+                Logger.LogError("Client is unavailable");
+                return;
+            }
+
+            try {
+                client.EndDisconnect(result);
+            } catch (Exception ex) {
+                Logger.LogError(ex, "Encountered an error while ending disconnect from {Server}", client.RemoteEndPoint.ToString());
+            } finally {
+                _event.Set();
+            }
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (!_disposed) {
+                if (disposing) {
+                    _event.Dispose();
+                }
+
+                _disposed = true;
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
